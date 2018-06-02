48557280c180764acb9b2ec7b4aac799e34ba153
Taqwa <trushdan@gmail.com>
2018-06-01 21:45:20 -0400

    Update userpoints-api-routes.js

--- app/routes/userpoints-api-routes.js
+++ app/routes/userpoints-api-routes.js
@@ -11,23 +11,21 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-console.log("***UserPoints***: ", db.Points);
+  console.log("***UserPoints***: ", db.Points);
 
-// GET route for all userPoints
-app.get("/api/userPoints", function(req, res) {
-  db.Points.findAll({})
-    .then(function(dbPoints) {
-      res.json(dbPoints);
-    });
-});
+  // GET route for all userPoints
+  app.get("/api/userPoints", function(req, res) {
+    db.Points.findAll({})
+      .then(function(dbPoints) {
+        res.json(dbPoints);
+      });
+  });
 
-// POST route for userPoints
-app.post("/api/userPoints", function(req, res) {
-  db.Points.create(req.body)
-    .then(function(dbPoints) {
-      res.json(dbPoints);
-});
-});
-
-
+  // POST route for userPoints
+  app.post("/api/userPoints", function(req, res) {
+    db.Points.create(req.body)
+      .then(function(dbPoints) {
+        res.json(dbPoints);
+      });
+  });
 };
